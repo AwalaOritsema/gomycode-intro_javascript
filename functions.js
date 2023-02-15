f284e4a192f5be031553e7e4f6940298f57635ed
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-15 20:07:30 +0100

    first commit

---
+++ functions.js
@@ -0,0 +1,31 @@
+// this lesson is about function in JavaScript
+// a function is a bunch of statements and expressions that produce a result
+let firstName = "Michael"
+
+function sayMyName(name) {
+  console.log("Your name is:", name)
+}
+
+firstName = "Anthonia"
+sayMyName(firstName)
+
+// function addTwo(number) {
+//   let result = number + 2
+//   return result
+// }
+
+function doSomething() {
+  console.log("I am doing something")
+
+  return 100
+}
+
+let hundred = doSomething()
+console.log(hundred)
+
+// arrow function
+let addTwo = (number) => number + 2
+
+let myValue = 80
+let expectedResult = addTwo(myValue)
+console.log(expectedResult)
---
+++ script.js
@@ -0,0 +1,77 @@
+// let costPrice = 100
+// let sellingPrice = 120
+
+// let profit = sellingPrice - costPrice
+// console.log(profit)
+
+// costPrice = 200
+// profit = sellingPrice - costPrice
+// console.log(profit)
+
+// let secondsInAMinute = 60
+// let secondsInTenMinutes = secondsInAMinute * 10
+// console.log(secondsInTenMinutes)
+
+// let daysInAYear = 365
+// let daysInAWeek = daysInAYear / 51
+// console.log(daysInAWeek)
+
+// costPrice = 300
+// let newCostPrice = 300 + 150
+// console.log(newCostPrice)
+
+// let firstName
+// console.log(firstName)
+
+// firstName = "Michael"
+// console.log(firstName)
+
+// // defining a variable
+// let variableName = "some value"
+// console.log(variableName)
+
+// // defining a constant
+// const constantName = "some constant"
+// console.log(constantName)
+
+// variableName = "another value"
+// console.log(variableName)
+
+// constantName = "another constant"
+
+// Data Types
+
+// // number
+// let myNumber = 157
+// console.log("A number type:", myNumber)
+
+// // string
+// let myString = "Hello World"
+// console.log("A string type:", myString)
+
+// // boolean
+// let myBoolean = true
+// console.log("A boolean type:", myBoolean)
+
+// // Array
+// let myArray = [false, 1, 2, 3, "Michael", 5]
+// console.log("An Array type:", myArray)
+
+// // object
+let myObject = {
+  name: "Michael",
+  score: 70,
+  walk() {
+    console.log("walking...")
+  },
+}
+// console.log("An object type:", myObject)
+// console.log("my object properties:")
+// console.log(myObject.name)
+// console.log(myObject.score)
+// myObject.walk()
+
+// SOME STRING PROPERTIES AND METHODS
+let myName = "michael"
+console.log(myName.length)
+console.log(myName.toUpperCase())
