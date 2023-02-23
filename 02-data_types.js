b9b11dea785f691bb2e26f17140d7f7da35afc3c
Michael Peter <michaelpeter.ai@gmail.com>
2023-02-23 22:15:03 +0100

    data types

---
+++ 02-data_types.js
@@ -0,0 +1,38 @@
+// number type
+
+// VARIABLE
+// x = 100
+// firstNumber = 100
+let firstNumber
+firstNumber = 100
+// let firstNumber = 100
+firstNumber = 300
+console.log("first number is:", firstNumber)
+
+// CONSTANT
+// y = 50
+// secondNumber = 50
+const secondNumber = 50
+console.log("second number is:", secondNumber)
+
+// string type
+let firstName = "Michael"
+console.log("first name is:", firstName)
+
+// boolean type
+let isLoggedIn = false
+console.log(isLoggedIn)
+
+// undefined type
+let lastName = "Peter"
+console.log("last name is:", lastName)
+
+lastName = undefined
+console.log("last name is:", lastName)
+
+// null type
+let score = 100
+console.log("score is:", score)
+
+score = null
+console.log("score is:", score)
