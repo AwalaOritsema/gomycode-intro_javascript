a49305d861273d46a18c4d92f672b1ecf6d375b0
Michael Peter <michaelpeter.ai@gmail.com>
2023-03-07 21:04:50 +0100

    loops in javascript with for loop

---
+++ 07-loops.js
@@ -0,0 +1,22 @@
+// array data structure -> iterable
+const numbers = [
+  10, 32, 87, 67, 90, 12, 11, 13, 15, 54, 67, 83, 99, 01, 28, 33, 34, 92,
+]
+
+// types of loops in JS
+// for-loop
+// while-loop
+// do-while-loop
+
+// for loop
+// for (let i = 0; i < 5; i++) {
+//   console.log("hello world")
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(`${i}th element is ${numbers[i]}`)
+// }
+
+// for (const number of numbers) {
+//   console.log(`Number is ${number}`)
+// }
