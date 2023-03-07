cd5452a469900b08b0fbddb3891654e77062626a
Michael Peter <michaelpeter.ai@gmail.com>
2023-03-07 19:31:08 +0100

    migrated if else codes to new file

--- 05-arrays.js
+++ 05-arrays.js
@@ -19,16 +19,3 @@ const eighthOrange = oranges[8]
 // console.log("total price of oranges:", totalPrice)
 // console.log("third orange price is", thirdOrange)
 // console.log("eighth orange price is", eighthOrange)
-
-function getNumberType(number) {
-  if (number % 2 === 0) {
-    console.log(`${number} is even`)
-  } else {
-    console.log(`${number} is odd`)
-  }
-}
-
-getNumberType(10)
-getNumberType(5)
-getNumberType(78)
-getNumberType(11)
---
+++ 06-if_else.js
@@ -0,0 +1,12 @@
+function getNumberType(number) {
+  if (number % 2 === 0) {
+    console.log(`${number} is even`)
+  } else {
+    console.log(`${number} is odd`)
+  }
+}
+
+getNumberType(10)
+getNumberType(5)
+getNumberType(78)
+getNumberType(11)
