c8bd2171d5d7f416f81f64decbb74df4fe7d8c79
Michael Peter <michaelpeter.ai@gmail.com>
2023-03-07 20:19:09 +0100

    if else and switch case

--- 06-if_else.js
+++ 06-if_else.js
@@ -6,7 +6,49 @@ function getNumberType(number) {
   }
 }
 
-getNumberType(10)
-getNumberType(5)
-getNumberType(78)
-getNumberType(11)
+// getNumberType(10)
+// getNumberType(5)
+// getNumberType(78)
+// getNumberType(11)
+
+// Calculating students' grades
+// 70 - 100 -> A
+// 60 - 69 -> B
+// 50 - 59 -> C
+// 40 - 49 -> D
+// 0 - 39 -> F
+
+function getGrade(score) {
+  //   if (score >= 70) {
+  //     console.log("Grade is: A")
+  //   } else if (score >= 60) {
+  //     console.log("Grade is: B")
+  //   } else if (score >= 50) {
+  //     console.log("Grade is: C")
+  //   } else if (score >= 40) {
+  //     console.log("Grade is: D")
+  //   } else {
+  //     console.log("Grade is: F")
+  //   }
+  switch (true) {
+    case score >= 70:
+      console.log("Grade is: A")
+      break
+    case score >= 60:
+      console.log("Grade is: B")
+      break
+    case score >= 50:
+      console.log("Grade is: C")
+      break
+    case score >= 40:
+      console.log("Grade is: D")
+      break
+    default:
+      console.log("Grade is: F")
+  }
+}
+
+getGrade(91)
+getGrade(28)
+// getGrade(61)
+// getGrade(89)
