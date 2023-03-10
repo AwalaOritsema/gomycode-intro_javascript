a644c3792d3f3a22321ba71ef4ddb7b1b6eed731
Michael Peter <michaelpeter.ai@gmail.com>
2023-03-10 21:29:05 +0100

    frequency algorithm mental model with comments

---
+++ 10-frequency.js
@@ -0,0 +1,15 @@
+// function
+// object
+// loops
+
+// 1. Create an empty [frequency] object that should hold the elements of the
+// array as keys and the number of times they appear as values
+
+// 2. Loop over all the elements of the array
+// 2.1. For each element: check if that element is a key that
+// exists in the [frequency] object
+// 2.1.1. If element is a key: increment the value of that key by 1
+// If element is not a key: create a new property with the element
+// as key and 1 as the [initial] value
+
+// 3. Return the object
